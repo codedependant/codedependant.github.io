ce103a7c7d280faa067a9f11c6e8f7892e2a8e2e
Che' Nxusani <che.nxusani@gmail.com>
2014-10-30 16:54:10 +0100

    hidding post and portfolio in the meantime

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,14 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+
+var imageop = require('gulp-image-optimization');
+
+gulp.task('images', function(cb) {
+    gulp.src(['_src/images/**/*.png','_src/images/**/*.jpg','_src/images/**/*.gif','_src/images/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('images')).on('end', cb).on('error', cb);
+});
